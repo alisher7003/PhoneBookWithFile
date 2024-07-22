38d8a630a3916e06a27a431a81a5f6d8fe7c288d
alisher7003 <alisher.ishniyozov@gmail.com>
2024-07-22 21:30:03 +0500

    Created Project.

--- PhoneBookWithFile/Program.cs
+++ PhoneBookWithFile/Program.cs
@@ -5,8 +5,7 @@ namespace PhoneBookWithFile
     internal class Program
     {
         static void Main(string[] args)
-        {
-            
+        {            
             FileService fileService = new FileService();
             fileService.ShowMenu();
         }
